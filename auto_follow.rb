2ca6b0b3cd1ae11848dbb593ebb05995fe81bd97
pr0ject[404] <43380493+pr0ject404@users.noreply.github.com>
2018-10-21 19:21:19 -0300

    Update auto_follow.rb

--- auto_follow.rb
+++ auto_follow.rb
@@ -4,9 +4,10 @@ require 'rb-readline' # Ruby IRB
 require 'awesome_print' # Console output
 require_relative 'credentials' # Pulls in login credentials from credentials.rb
 
+
 username = $username
 password = $password
-users = ["josephineskriver", "theweeknd", "kendalljenner", "karliekloss"]
+users = ['whinderssonnunes', 'maisa', 'luisasonza', 'larissamanoela', 'bela_fernandesoficial', 'kevinho', 'bel_moreira', 'biancaandradeoficial', 'flaviapavanelli', 'instagram', 'selenagomez', 'arianagrande', 'taylorswift', 'beyonce', 'kimkardashian', 'cristiano', 'kyliejenner', 'justinbieber', 'kendalljenner', 'nickiminaj', 'natgeo', 'neymarjr', 'nike', 'leomessi', 'khloekardashian', 'katyperry', 'jlo', 'ddlovato', 'kourtneykardash', 'badgalriri', 'fcbarcelona', 'realmadrid', 'theellenshow', 'justintimberlake', 'zendaya', '9gag', 'chrisbrownofficial', 'vindiesel', 'champagnepapi', 'davidbeckham', 'shakira', 'gigihadid', 'emmawatson', 'kingjames', 'garethbale11', 'nikefootball', 'adele', 'zacefron', 'vanessahudgens', 'ladygaga', 'maluma', 'nba', 'nasa', 'luissuarez9', 'zayn', 'shawnmendes', 'adidasfootball', 'brumarquezine', 'hm', 'harrystyles', 'chanelofficial', 'ayutingting92', 'letthelordbewithyou', 'niallhoran', 'anitta', 'hudabeauty', 'adidasoriginals', 'marinaruybarbosa', 'lucyhale', 'karimbenzema', 'princessyahrini', 'zara', 'nickyjampr', 'onedirection', 'andresiniesta8', 'raffinagita1717', 'krisjenner', 'manchesterunited', 'natgeotravel', 'deepikapadukone', 'snoopdogg', 'davidluiz_4', '3030oficial', 'priyankachopra', 'ashleybenson', 'lelepons', 'prillylatuconsina96', 'louisvuitton', 'britneyspears', 'jbalvin', 'laudyacynthiabella', 'ciara', 'stephencurry30', 'instagrambrasil']
 follow_counter = 0
 unfollow_counter = 0
 MAX_UNFOLLOWS = 200
@@ -22,7 +23,7 @@ browser.text_field(:name => "username").set "#{username}"
 browser.text_field(:name => "password").set "#{password}"
 
 # Click Login Button
-browser.button(:class => '_ah57t _84y62 _i46jh _rmr7s').click
+browser.button(:class => 'oF4XW sqdOP  L3NKy      ').click
 sleep(2)
 puts "We're in #hackerman"
 
@@ -33,16 +34,22 @@ loop do
     browser.goto "instagram.com/#{val}/"
 
     # If not following then follow
-    if browser.button(:class => '_ah57t _84y62 _frcv2 _rmr7s').exists?
-      ap "Following #{val}"
-      browser.button(:class => '_ah57t _84y62 _frcv2 _rmr7s').click
+    if browser.button(:class => '_5f5mN       jIbKX  _6VtSN     yZn4P   ').exists?
+      ap "Following [ #{val} ]"
+	  sleep(1)
+      browser.button(:class => '_5f5mN       jIbKX  _6VtSN     yZn4P   ').click
+	  sleep(1.0/2.0)
       follow_counter += 1
-    elsif browser.button(:class => '_ah57t _6y2ah _frcv2 _rmr7s').exists?
-      ap "Unfollowing #{val}"
-      browser.button(:class => '_ah57t _6y2ah _frcv2 _rmr7s').click
-      unfollow_counter += 1
+    elsif browser.button(:class => '_5f5mN    -fzfL     _6VtSN     yZn4P   ').exists?
+      ap "Unfollowing [ #{val} ]"
+	  sleep(1)
+      browser.button(:class => '_5f5mN    -fzfL     _6VtSN     yZn4P   ').click
+	  sleep(1)
+	  browser.button(:class => 'aOOlW -Cab_   ').click
+	  sleep(1)
+      follow_counter -= 1
     end
-    sleep(1.0/2.0) # Sleep half a second to not get tripped up when un/following many users at once
+    sleep(1) # Sleep half a second to not get tripped up when un/following many users at once
   }
   puts "--------- #{Time.now} ----------"
   break if unfollow_counter >= MAX_UNFOLLOWS
@@ -54,6 +61,3 @@ ap "Followed #{follow_counter} users and unfollowed #{unfollow_counter} in #{((T
 # Leave this in to use the REPL at end of program
 # Otherwise, take it out and program will just end
 Pry.start(binding)
-
-# Top 100 users on Instagram
-# users = ['instagram', 'selenagomez', 'arianagrande', 'taylorswift', 'beyonce', 'kimkardashian', 'cristiano', 'kyliejenner', 'justinbieber', 'kendalljenner', 'nickiminaj', 'natgeo', 'neymarjr', 'nike', 'leomessi','khloekardashian', 'mileycyrus', 'katyperry', 'jlo', 'ddlovato', 'kourtneykardash', 'victoriasecret', 'badgalriri', 'fcbarcelona', 'realmadrid', 'theellenshow', 'justintimberlake', 'zendaya' 'caradelevingne', '9gag', 'chrisbrownofficial', 'vindiesel', 'champagnepapi', 'davidbeckham', 'shakira', 'gigihadid', 'emmawatson', 'jamesrodiguez10', 'kingjames', 'garethbale11', 'nikefootball', 'adele', 'zacefron', 'vanessahudgens', 'ladygaga', 'maluma', 'nba', 'nasa', 'rondaldinho', 'luissuarez9', 'zayn', 'shawnmendes', 'adidasfootball', 'brumarquezine', 'hm', 'harrystyles','chanelofficial', 'ayutingting92', 'letthelordbewithyou', 'niallhoran', 'anitta', 'hudabeauty', 'camerondallas', 'adidasoriginals', 'marinaruybarbosa', 'lucyhale', 'karimbenzema', 'princessyahrini', 'zara', 'nickyjampr', 'onedirection', 'andresiniesta8', 'raffinagita1717', 'krisjenner', 'manchesterunited', 'natgeotravel', 'marcelottwelve', 'deepikapadukone', 'snoopdogg', 'davidluiz_4', 'kalbiminrozeti', 'priyankachopra', 'ashleybenson', 'shaym', 'lelepons', 'prillylatuconsina96','louisvuitton','britneyspears', 'sr4official', 'jbalvin', 'laudyacynthiabella', 'ciara', 'stephencurry30', 'instagrambrasil']
