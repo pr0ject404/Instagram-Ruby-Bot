317a24505b8108d93c61a6d1c918cfdc170f378a
pr0ject[404] <43380493+pr0ject404@users.noreply.github.com>
2018-11-06 12:00:46 -0200

    Create Automatization.bat

---
+++ Automatization.bat
@@ -0,0 +1,44 @@
+@echo off
+
+:VRIFY
+Echo:
+Echo  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Echo: 
+Echo    1) Auto Follow and Un Follow
+Echo    2) Auto Liker
+Echo    3) Auto Un Follow ( profiles set on config.rb )
+Echo    3) EXIT
+Echo: 
+Echo  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Echo:
+choice /c 123
+if errorlevel 4 EXIT
+if errorlevel 3 goto AUNF
+if errorlevel 2 goto ALIK
+if errorlevel 1 goto AFUNF
+
+:AFUNF
+auto_follow.rb
+cls
+echo restarting...
+timeout 5
+cls
+goto AFUNF
+
+:ALIK
+auto_liker.rb
+cls
+echo finishing...
+timeout 10
+cls
+Echo:
+Echo Auto Liker Finished.
+goto VRIFY
+
+:AUNF
+unfollow.rb
+cls
+echo restarting...
+timeout 5
+cls
+goto AUNF
